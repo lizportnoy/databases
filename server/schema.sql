b00bfd8ef12184fd6272ec9acb1944a400cee38e
Liz Portnoy <lizport10@gmail.com>
2014-11-21 13:20:54 -0800

    initial schema

--- server/schema.sql
+++ server/schema.sql
@@ -2,12 +2,31 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
+CREATE TABLE Messages (
   /* Describe your table here.*/
+  MessageID INTEGER PRIMARY KEY,
+  Message VARCHAR(400) NOT NULL
 );
 
 /* Create other tables and define schemas for them here! */
+CREATE TABLE Users (
+  UserID INTEGER PRIMARY KEY,
+  Username VARCHAR(50) NOT NULL
+);
 
+CREATE TABLE Rooms (
+  RoomID INTEGER PRIMARY KEY,
+  Text VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Mapping (
+  MappingID INTEGER PRIMARY KEY,
+  UserId INTEGER NOT NULL,
+  ExamId INTEGER NOT NULL,
+  Score NUMERIC NOT NULL,
+  FOREIGN KEY(StudentId) REFERENCES Student(StudentId),
+  FOREIGN KEY(ExamId) REFERENCES Exam(ExamId)
+);
 
 
 
